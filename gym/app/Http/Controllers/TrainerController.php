c52bf1f35ae9f829e466f1bf0b5e420d84f687d3
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-06-17 13:21:39 +0200

    created TrainerController.php endpoints

--- gym/app/Http/Controllers/TrainerController.php
+++ gym/app/Http/Controllers/TrainerController.php
@@ -2,7 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+
 class TrainerController extends UserController
 {
 
+    public function getAll()
+    {
+        $trainers = User::where('role', 'trainer')->get();
+        return response()->json($trainers);
+    }
+
+    public function getById(int $id)
+    {
+        $trainer = User::where('role', 'trainer')->where('id', $id)->first();
+        return response()->json($trainer);
+    }
 }
