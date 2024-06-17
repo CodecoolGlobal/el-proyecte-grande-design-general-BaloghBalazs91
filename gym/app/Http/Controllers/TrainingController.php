ea6118fa47bf167655408db132349a7c0b2174ed
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-06-17 13:21:57 +0200

    created TrainerController.php endpoints

--- gym/app/Http/Controllers/TrainingController.php
+++ gym/app/Http/Controllers/TrainingController.php
@@ -3,18 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Models\Training;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class TrainingController extends Controller
 {
-    public function getAll()
+    public function getAll(Request $request)
     {
-        $trainings = Training::all() ?? [];
+        $week = $request->query('week', 0);
+
+        $currentDate = Carbon::now();
+        $startOfWeek = $currentDate->addWeeks((int)$week)->startOfWeek()->startOfDay()->toDateTimeString();
+        $endOfWeek = $currentDate->addWeeks((int)$week)->endOfWeek()->endOfDay()->toDateTimeString();
+
+        $trainings = Training::whereBetween('start', [$startOfWeek, $endOfWeek])->get();
 
         return response()->json($trainings);
     }
 
-    public function getById($id)
+    public function getById(int $id)
     {
         $training = Training::query()->find($id);
         return response()->json($training);
--- gym/routes/web.php
+++ gym/routes/web.php
@@ -20,9 +20,19 @@ Route::post('register', [UserController::class, 'registerTrainee']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('user.profile', [UserController::class, 'profile'])->name('profile');
+
+    # to implement
+    # Route::put('trainings/book/{id}', [\App\Http\Controllers\TrainingController::class, 'JoinTrainingById']);
 });
 
 Route::get('training-methods', [\App\Http\Controllers\TrainingMethodController::class, 'getAll']);
 Route::get('training-methods/{training_method_name}', [\App\Http\Controllers\TrainingMethodController::class, 'getByName']);
 
+// Trainings
+Route::get('trainings', [\App\Http\Controllers\TrainingController::class, 'getAll']);
+Route::get('trainings/{id}', [\App\Http\Controllers\TrainingController::class, 'getById']);
+
+// Trainers
+Route::get('trainers', [\App\Http\Controllers\TrainerController::class, 'getAll']);
+Route::get('trainers/{id}', [\App\Http\Controllers\TrainerController::class, 'getById']);
 
