3c380ccbc8d3a5cff438d3179bc9e413c82978ca
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-07-18 11:01:33 +0200

    Binded interfaces to AppServiceProvider.php

--- gym/app/Providers/AppServiceProvider.php
+++ gym/app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Repositories\TrainerRepository\TrainerRepository;
+use App\Repositories\TrainerRepository\TrainerRepositoryInterface;
+use App\Repositories\TrainingMethodRepository\TrainingMethodRepository;
+use App\Repositories\TrainingMethodRepository\TrainingMethodRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->app->bind(TrainerRepositoryInterface::class, TrainerRepository::class);
+        $this->app->bind(TrainingMethodRepositoryInterface::class, TrainingMethodRepository::class);
     }
 }
