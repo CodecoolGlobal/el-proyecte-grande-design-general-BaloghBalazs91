01e0e488f4be09b856073ad607053e12a072e068
BaloghBalazs91 <blay1991@gmail.com>
2024-07-18 11:00:55 +0200

    TrainingRepositoryInterface.php created

---
+++ gym/app/Repositories/TrainingRepositoryInterface.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Repositories;
+use App\Models\Training;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+interface TrainingRepositoryInterface{
+    public function getall();
+    public function getTrainingsByWeek(?string $week);
+    public function createTraining();
+    public function showTraining(Training $training);
+    public function storeTraining(Training $training);
+    public function editTraining(Training $training);
+    public function updateTraining(Training $training);
+    public function deleteTraining(Training $training);
+    public function joinTrainingById(User $user, Training $training);
+    public function getTraniees(User $user, Training $training);
+
+}
